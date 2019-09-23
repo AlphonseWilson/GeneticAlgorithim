28565769e498e338feb497dc311f7efac6b31d64
Alphonse Wilson <Alphonse.Wilson.software@gmail.com>
2019-09-22 19:00:08 -0700

    added rendering

--- src/com/alphonseShyam/driver/Driver.java
+++ src/com/alphonseShyam/driver/Driver.java
@@ -9,11 +9,57 @@ public class Driver extends Canvas implements Runnable{
 	private static final long serialVersionUID = 1467625635353985708L;
 
 	
+	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+	private Thread thread;
+	private boolean running = false;
+	public Driver() {
+		new Window(WIDTH, HEIGHT, "Lets preform a genetic algorithm", this);
+	}
 	
 	public synchronized void start() {
-		
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	public synchronized void stop() {
+		try {
+			thread.join();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				delta--;
+				
+			}
+			if (running) {
+				render();
+			}
+			frames++;
+			if(System.currentTimeMillis() -timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: "+frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	private void tick() {
+		
+	}
+	private void render() {
 		
 	}
 	public static void main(String args[]) {
